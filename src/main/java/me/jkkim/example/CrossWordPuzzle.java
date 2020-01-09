eaf64083ce692bce54123cf62122c57e8f13e129
Jung-gyum KIM <jgkim@lviscorp.com>
2020-01-09 16:37:35 +0900

    [2] Solve CrossWord puzzle
    * Double for loop to find out the position of common character.
    * It works even when no common character exists.

--- src/main/java/me/jkkim/example/CrossWordPuzzle.java
+++ src/main/java/me/jkkim/example/CrossWordPuzzle.java
@@ -1,9 +1,40 @@
 package me.jkkim.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class CrossWordPuzzle {
+
     static List<String> solve(String a, String b) {
-        return null;
+        char[] arrayA = a.toCharArray();
+        char[] arrayB = b.toCharArray();
+
+        int posA = -1;
+        int posB = -1;
+        for (int i = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayA[i] == arrayB[j]) {
+                    posA = i;
+                    posB = j;
+                    break;
+                }
+            }
+            if (0 <= posA) break;
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int j = 0; j < arrayB.length; j++) {
+            if (j == posB) {
+                result.add(a);
+            } else {
+                char[] row = new char[arrayA.length];
+                for (int i = 0; i < arrayA.length; i++) {
+                    row[i] = i == posA ? arrayB[j] : '.';
+                }
+                result.add(String.valueOf(row));
+            }
+        }
+
+        return result;
     }
 }
